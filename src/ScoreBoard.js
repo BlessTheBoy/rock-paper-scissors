d6da646a8367b259de1df819980993073177f2ba
Faruq Bakare <bakarefaruq.a@gmail.com>
2020-12-29 14:59:07 +0100

    styling scoreboard

--- src/ScoreBoard.js
+++ src/ScoreBoard.js
@@ -1,18 +1,19 @@
 import React from "react";
+import "./scss/ScoreBoard.scss";
 
 function ScoreBoard() {
   return (
     <div className="scoreBoard">
-      <p>SCORE</p>
+      <p className="scoreBoard__text">SCORE</p>
       <div className="scoreBoard__score">
         <div className="user">
           <p className="user__score">0</p>
-          <p>YOU</p>
+          <p className="user__name">YOU</p>
         </div>
         <p className="dash">-</p>
         <div className="computer">
           <p className="computer__score">0</p>
-          <p>COMPUTER</p>
+          <p className="computer__name">COMPUTER</p>
         </div>
       </div>
       <p className="ties">
